6cbf90c5ca1c09c626779b647a4caafd3153da08
Pavel Yanushevsky <AlaskanHusky@users.noreply.github.com>
2017-05-05 00:17:33 +0300

    Change model

--- models/atm.js
+++ models/atm.js
@@ -1,47 +1,52 @@
 var mongoose = require("mongoose"); // connect Mongoose (MongoDB ODM library)
-var moment = require("moment");
 var Schema = mongoose.Schema; // create Mongoose Schema
-// set region
-moment.locale('ru');
-//
+// connect promises
 mongoose.Promise = global.Promise;
 // atm scheme definition
 var ATMScheme = new Schema({
   bank: {
     type: String,
-    required: true,
+    required: true
   },
-  address: {
-    type: String,
-    required: true,
+  atm_info: {
+    id: {
+      type: Number,
+      unique: true,
+      required: true
+    },
+    address: {
+      type: String,
+      required: true
+    },
+    latitude: {
+      type: Number,
+      required: true
+    },
+    longitude: {
+      type: Number,
+      required: true
+    }
   },
-  latitude: {
-    type: Number,
-    unique: true,
-    required: true,
-  },
-  longitude: {
-    type: Number,
-    unique: true,
-    required: true,
-  },
-  er: {
-    values_list: {
-      type: [Schema.ObjectId],
-      ref: 'exchange_rate',
+  exchange_rates: {
+    currency_name: {
+      type: String,
+      min: 3,
+      max: 3,
       required: true,
     },
-    time: {
+    purchase_value: {
+      type: Number,
+      required: true,
+    },
+    sale_value: {
+      type: Number,
+      required: true,
+    },
+    date: {
       type: String,
       required: true,
     }
   }
-});
-
-ATMScheme
-.virtual('currencies_rate_time_formatted')
-.get(function () {
-  return moment(this.er.time, 'HH:mm DD.MM.YYYY');
 });
 
 module.exports = mongoose.model('ATM', ATMScheme); // export model
